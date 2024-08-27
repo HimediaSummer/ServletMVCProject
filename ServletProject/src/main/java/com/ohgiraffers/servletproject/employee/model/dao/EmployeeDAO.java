c97adba403679fe79266709d1bd6413449239872
summerchoco <shkkk90@daum.net>
2024-08-27 10:59:53 +0900

    first push insert

--- ServletProject/src/main/java/com/ohgiraffers/servletproject/employee/model/dao/EmployeeDAO.java
+++ ServletProject/src/main/java/com/ohgiraffers/servletproject/employee/model/dao/EmployeeDAO.java
@@ -12,7 +12,53 @@ import java.util.Properties;
 
 import static com.ohgiraffers.servletproject.common.jdbc.JDBCTemplate.close;
 
-/**/
 public class EmployeeDAO {
+
+    private final Properties prop;
+
+    public EmployeeDAO() {
+
+        prop = new Properties();
+        try {
+            prop.loadFromXML(new FileInputStream(ConfigLocation.MAPPER_LOCATION + "employee-mapper.xml"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // insert 사원등록
+    public int insertEmp(Connection con, EmployeeDTO emp) {
+
+        PreparedStatement pstmt = null;
+
+        int result = 0;
+
+        String query = prop.getProperty("insertEmp");
+
+        try {
+            pstmt = con.prepareStatement(query);
+            pstmt.setString(1, emp.getEmpId());
+            pstmt.setString(2, emp.getEmpName());
+            pstmt.setString(3, emp.getEmpNo());
+            pstmt.setString(4, emp.getEmail());
+            pstmt.setString(5, emp.getPhone());
+            pstmt.setString(6, emp.getDeptCode());
+            pstmt.setString(7, emp.getJobCode());
+            pstmt.setString(8, emp.getSalLevel());
+            pstmt.setInt(9, emp.getSalary());
+            pstmt.setDouble(10, emp.getBonus());
+            pstmt.setString(11, emp.getManagerId());
+            pstmt.setDate(12, emp.getHireDate());
+
+            result = pstmt.executeUpdate();
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            close(pstmt);
+        }
+
+        return result;
+    }
 
 }
--- ServletProject/src/main/java/com/ohgiraffers/servletproject/employee/model/service/EmployeeService.java
+++ ServletProject/src/main/java/com/ohgiraffers/servletproject/employee/model/service/EmployeeService.java
@@ -11,4 +11,43 @@ import static com.ohgiraffers.servletproject.common.jdbc.JDBCTemplate.*;
 /**/
 public class EmployeeService {
 
+    private final EmployeeDAO empDAO;
+
+    public EmployeeService() {
+        empDAO = new EmployeeDAO();
+    }
+
+    public EmployeeDTO selectOneEmpById(String empId) {
+
+        /* Connection 생성 */
+        Connection con = getConnection();
+
+        /* Connection과 함께 정보를 전달하여 조회한다. */
+        EmployeeDTO selectedEmp = empDAO.selectEmpById(con, empId);
+
+        /* connection 닫기 */
+        close(con);
+
+        /* 조회 결과를 반환한다. */
+        return selectedEmp;
+    }
+
+    /* 신규 사원 등록용 메소드 */
+    public int insertEmp(EmployeeDTO emp) {
+
+        Connection con = getConnection();
+
+        int result = empDAO.insertEmp(con, emp);
+
+        if (result > 0) {
+            commit(con);
+        } else {
+            rollback(con);
+        }
+
+        close(con);
+
+        return result;
+    }
+
 }
