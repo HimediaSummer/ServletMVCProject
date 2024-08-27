9f0adf23515bda605249923841ee71660d9402ab
BJY10707 <bjy10707@gmail.com>
2024-08-27 15:06:27 +0900

    merger Last update

--- ServletProject/src/main/java/com/ohgiraffers/servletproject/employee/model/service/EmployeeService.java
+++ ServletProject/src/main/java/com/ohgiraffers/servletproject/employee/model/service/EmployeeService.java
@@ -11,4 +11,37 @@ import static com.ohgiraffers.servletproject.common.jdbc.JDBCTemplate.*;
 /**/
 public class EmployeeService {
 
+    private final EmployeeDAO empDAO;
+
+    public EmployeeService() {
+        empDAO = new EmployeeDAO();
+    }
+
+    public List<EmployeeDTO> selectAllEmp() {
+
+        Connection con = getConnection();
+
+        List<EmployeeDTO> empList = empDAO.selectAllEmpList(con);
+
+        close(con);
+
+        return empList;
+
+    }
+
+    public int deleteEmp(String empId) {
+        Connection con = getConnection();
+
+        int result = empDAO.deleteEmp(con, empId);
+
+        if (result > 0) {
+            commit(con);
+        } else {
+            rollback(con);
+        }
+
+        close(con);
+
+        return result;
+    }
 }
